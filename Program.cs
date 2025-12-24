8b60fd28c80c010d988adae32ae3f11dc6510dc9
MustafaSzr-16 <162330853+MustafaSzr-16@users.noreply.github.com>
2025-12-24 21:24:05 +0300

    Array Methods

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿// dizi metotları (Array Methods)
+
+
+string[] sehirler = {"zonguldak","rize","kocaeli"};
+int[] plakalar = {67,53,41};
+
+// sehirler[0] = "sakarya";
+//sehirler.SetValue("sakarya",1);
+
+Console.WriteLine(sehirler.GetValue(1));
+Console.WriteLine(sehirler.Length);
+Console.WriteLine(Array.IndexOf(sehirler,"amasya"));
+
+Array.Sort(sehirler);    //alfabetik şekilde sıralar 
+Array.Sort(plakalar);    // küçükten büyüğe sıralar
+
+Array.Reverse(plakalar);    //diziyi ters çevirir.
+
+Array.Clear(sehirler);
+Array.Clear(plakalar,0,1);
+
+Console.WriteLine(plakalar.GetValue(0));
+Console.WriteLine(plakalar.GetValue(1));
+Console.WriteLine(plakalar.GetValue(2));
+
+
