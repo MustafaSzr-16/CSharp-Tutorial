b44c8b5aacc92d8a723b7ba490c994bea050a9ed
MustafaSzr-16 <162330853+MustafaSzr-16@users.noreply.github.com>
2025-12-25 18:40:52 +0300

    Array Slicing

--- Program.cs
+++ Program.cs
@@ -1,26 +1,12 @@
-﻿// dizi metotları (Array Methods)
+﻿// Array Slicing
 
+string[] sehirler = {"zonguldak","rize","kocaeli","istanbul","ankara","çanakkale"};
 
-string[] sehirler = {"zonguldak","rize","kocaeli"};
-int[] plakalar = {67,53,41};
+foreach(var sehir in sehirler[2..])
+{
+    Console.WriteLine(sehir);
+}    
 
-// sehirler[0] = "sakarya";
-//sehirler.SetValue("sakarya",1);
+string il = "kocaeli";
 
-Console.WriteLine(sehirler.GetValue(1));
-Console.WriteLine(sehirler.Length);
-Console.WriteLine(Array.IndexOf(sehirler,"amasya"));
-
-Array.Sort(sehirler);    //alfabetik şekilde sıralar 
-Array.Sort(plakalar);    // küçükten büyüğe sıralar
-
-Array.Reverse(plakalar);    //diziyi ters çevirir.
-
-Array.Clear(sehirler);
-Array.Clear(plakalar,0,1);
-
-Console.WriteLine(plakalar.GetValue(0));
-Console.WriteLine(plakalar.GetValue(1));
-Console.WriteLine(plakalar.GetValue(2));
-
-
+Console.WriteLine(il[..5]);
